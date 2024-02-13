c7bd43d68ea5dc8367039a8a6556dae7f4a7dee7
shawal-mbalire <mbalireshawal@gmail.com>
2024-02-13 11:06:43 +0300

    j

--- GreenHub/momoapi/greenhub_trx_flask/app.py
+++ GreenHub/momoapi/greenhub_trx_flask/app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask  import Flask
+from dotenv import load_dotenv
 
 import os
 import psycopg2 
-from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL)
+DATABASE_NAME     = os.environ['DATABASE_NAME']
+DATABASE_USER     = os.environ['DATABASE_USER']
+DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+DATABASE_HOST     = os.environ['DATABASE_HOST']
+DATABASE_PORT     = os.environ['DATABASE_PORT']
+
+conn = psycopg2.connect(
+    dbname   = DATABASE_NAME,
+    user     = DATABASE_USER,
+    password = DATABASE_PASSWORD,
+    host     = DATABASE_HOST,
+    port     = DATABASE_PORT
+
+)
 
 @app.route('/')
 def hello_world():
